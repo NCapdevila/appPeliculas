da9b76fa28424a5560e567d871f5bd31403f9979
Ramiro Capdevila <ramirocapdevila23@gmail.com>
2023-03-04 01:25:37 -0300

    agrego hooks

--- src/components/MoviesGrid/MoviesGrid.jsx
+++ src/components/MoviesGrid/MoviesGrid.jsx
@@ -1,7 +1,22 @@
 import style from"./MoviesGrid.module.css";
-import movies from "../../data/movies.json";
 import MovieCard from "../MovieCard/MovieCard";
+import { useEffect, useState } from "react";
+
 function MoviesGrid() {
+    const [movies, setMovies] = useState([]);
+    
+
+    useEffect(() =>{
+        fetch("https://api.themoviedb.org/3/discover/movie", {
+            headers:{
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTBhMmE1NzVmNjkxNDQwZWJiYWVmMGM5M2JiMGJhZiIsInN1YiI6IjYzZjAzOWRhMTUzNzZjMDA3ODE3YzNjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wR-pwUm2v3m8-5NpHKl3Wn_KmXEfRSkME6lhvafsF2k",
+                "Content-Type":  "application/json;charset=utf-8'"
+            },
+        }).then(result => result.json().then(data => {
+           setMovies(data.results)
+        }));
+    },[]);
+
     return (
         <ul className={style.moviesGrid}>
             {movies.map((movie) => (
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,13 +1,32 @@
-import movie from './movie.json'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
 import styles from './MovieDetails.module.css'
 
 function MovieDetails(){
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+  
+  useEffect(()=>{
+    fetch("https://api.themoviedb.org/3/movie" + movieId, {
+            headers:{
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTBhMmE1NzVmNjkxNDQwZWJiYWVmMGM5M2JiMGJhZiIsInN1YiI6IjYzZjAzOWRhMTUzNzZjMDA3ODE3YzNjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wR-pwUm2v3m8-5NpHKl3Wn_KmXEfRSkME6lhvafsF2k",
+                "Content-Type":  "application/json;charset=utf-8'"
+            },
+        }).then(result => result.json()).then(data =>{
+          setMovie(data)
+        })    
+  },[movieId])
+
+  if(!movie){
+    return null;
+  }
+
     const imgUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path
     return(
         <div className={styles.detailsContainer}>
-           <img src={imgUrl} alt={movie.title} />
-           <div>
-            <p>
+           <img src={imgUrl} alt={movie.title} className={`${styles.col} ${styles.Movieimg}`}/>
+           <div className={`${styles.col} ${styles.MovieDetails}`}>
+            <p className={styles.FirtsItem}>
               <strong>Title: </strong> {movie.title}
             </p>
             <p>
